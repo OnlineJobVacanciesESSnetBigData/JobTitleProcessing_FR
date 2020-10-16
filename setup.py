d14f90b9ed422d90c865ed1ad9bb406843fabea6
claire2m <52279207+claire2m@users.noreply.github.com>
2020-10-16 13:00:14 +0200

    Change git link + version number + package full name.

--- setup.py
+++ setup.py
@@ -7,20 +7,19 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="job-title-processing-Dares",
-    version="0.0.1",
+    name="job-title-processing",
+    version="0.1.0",
     author="Dares",
     author_email="dares.projets-big-data@travail.gouv.fr'",
     description="A package to clean and label french job offers title",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://framagit.org/jocas/job_title_processing",
+    url="https://github.com/OnlineJobVacanciesESSnetBigData/JobTitleProcessing_FR",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
-
